430dc51ba40e9876e18fc5523fadb12b87b99f11
canyan7n <canyann@163.com>
2023-02-02 16:47:28 +0800

    fourth commit on idea

---
+++ src/main/java/com/canyan/GitTest.java
@@ -0,0 +1,10 @@
+package com.canyan;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.printf("hello,Git!");
+        System.out.printf("hello,Git2!");
+        System.out.printf("hello,Git3!");
+        System.out.printf("hello,Git4!");
+    }
+}
